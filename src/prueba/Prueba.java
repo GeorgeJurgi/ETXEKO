3ea2ad2fceed5e146328c059369b2b18d09d5f12
Gorka Ortega <gor_australia@hotmail.com>
2022-05-10 21:55:21 +0200

    He cambiado algo

---
+++ src/prueba/Prueba.java
@@ -0,0 +1,14 @@
+package prueba;
+
+public class Prueba {
+
+	public Prueba() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("hola git");
+
+	}
+
+}
